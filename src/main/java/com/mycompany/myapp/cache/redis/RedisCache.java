f7f0d0a7fc428997980064ed60984fb197359312
Anthony Viard <anth.viard@gmail.com>
2020-11-19 23:12:13 +0100

    Add Javadoc for Redis Cache

--- src/main/java/com/mycompany/myapp/cache/redis/RedisCache.java
+++ src/main/java/com/mycompany/myapp/cache/redis/RedisCache.java
@@ -11,6 +11,13 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 
+/**
+ * This class represent a cache manager for the given object T.
+ * Each object managed by the Redis cache should have a proper implementation
+ * and define a prefix through the constructor.
+ *
+ * @param <T> The object to handle in the cache
+ */
 public abstract class RedisCache<T> {
     public static final String NULL_KEYS_NOT_SUPPORTED_MSG = "Null keys are not supported";
 
@@ -22,9 +29,12 @@ public abstract class RedisCache<T> {
 
     final Class<T> type;
     /**
-     * The prefix used to define the key for storage purpose.
-     * It should have a delimiter, like : / - ... to make the key searching easier.
+     * The prefix is used to define the key for storing purpose.
+     * It should have a delimiter, like : / - ... to make the research easier.
      * e.g. "USER:"
+     *
+     * The value is define in the constructor and it's used in the generateKey() method
+     * or to retrieve all current keys in the cache through keys() method.
      */
     final String prefix;
 
@@ -35,15 +45,22 @@ public abstract class RedisCache<T> {
 
     /**
      * This method generate the key used to store an element in the cache.
-     * The identifier has to be unique to form an unique key combine with the prefix.
+     * The identifier has to be unique to form an unique key when it combines with the prefix.
      *
      * @param identifier an unique identifier for a given object to store.
-     * @return the key used to store the element in the store
+     * @return The key used to store the element in the store
      */
     final String generateKey(Object identifier) {
         return prefix + identifier;
     }
 
+    /**
+     * Return the value in the cache or launch the Supplier lambda if no value is present in the cache.
+     *
+     * @param identifier the unique object's identifier to store
+     * @param valueLoader a lambda used to load the value in case cache is empty
+     * @return The value found in the cache or retrieved by the supplier
+     */
     public Optional<T> get(Object identifier, Supplier<T> valueLoader) {
         if (identifier == null) {
             throw new NullPointerException(NULL_KEYS_NOT_SUPPORTED_MSG);
@@ -65,6 +82,12 @@ public abstract class RedisCache<T> {
         return Optional.ofNullable(result);
     }
 
+    /**
+     * Store an object in the cache.
+     *
+     * @param identifier the unique object's identifier to store (could not be null)
+     * @param value the value to store
+     */
     public void set(Object identifier, T value) {
         if (identifier == null) {
             throw new NullPointerException(NULL_KEYS_NOT_SUPPORTED_MSG);
@@ -81,10 +104,17 @@ public abstract class RedisCache<T> {
         }
     }
 
+    /**
+     * Remove all entries in the cache according to keys returned by the keys() method.
+     */
     public void clear() {
         evict(keys());
     }
 
+    /**
+     * Remove an element from the cache according to the given identifier
+     * @param identifier the unique object's identifier to remove
+     */
     public void evict(Object identifier) {
         if (identifier == null) {
             throw new NullPointerException(NULL_KEYS_NOT_SUPPORTED_MSG);
@@ -93,6 +123,10 @@ public abstract class RedisCache<T> {
         this.evict(Collections.singletonList(identifier));
     }
 
+    /**
+     * Remove elements from the cache according to the given identifiers
+     * @param identifiers a list of identifier to remove
+     */
     public void evict(List<Object> identifiers) {
         if (identifiers == null) {
             throw new NullPointerException(NULL_KEYS_NOT_SUPPORTED_MSG);
@@ -102,6 +136,12 @@ public abstract class RedisCache<T> {
         redis.del(finalKeys);
     }
 
+    /**
+     * Retrieve all keys from the cache according the prefix value.
+     * e.g. for User entity the searched term would be "USER:*"
+     *
+     * @return A List of keys from the cache
+     */
     public List<String> keys() {
         return redis.keys(prefix + "*").stream().map(Object::toString).collect(Collectors.toList());
     }
--- src/main/java/com/mycompany/myapp/cache/redis/UserRedisCache.java
+++ src/main/java/com/mycompany/myapp/cache/redis/UserRedisCache.java
@@ -4,6 +4,13 @@ import com.mycompany.myapp.domain.User;
 
 import javax.inject.Singleton;
 
+/**
+ * This cache manager is used to manage users in the Redis cache.
+ *
+ * An entry per user and per identifier.
+ *
+ * e.g. USER:admin or USER:admin@localhost (or anything you want)
+ */
 @Singleton
 public class UserRedisCache extends RedisCache<User> {
 
